2ce602a01a643fbb56269c616796a9d142fc43d3
duckytutorials <discordtutorialsworld@gmail.com>
2022-12-26 18:41:31 +0630

    Update log.js

--- package/functions/funcs/log.js
+++ package/functions/funcs/log.js
@@ -1,9 +1,9 @@
 module.exports = async d => {
     const code = d.command.code
-
+    const log = require('../../classes/Logger.js')
     const inside = d.unpack()
     
-    console.log(inside.addBrackets())
+    log.debug(inside.addBrackets())
     
     return {
         code: code.replaceLast(`$log${inside}`, "")
