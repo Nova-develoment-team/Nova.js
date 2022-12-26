a45ea4f6ac49e87c8e1bd225a4a8c7543f169df6
duckytutorials <discordtutorialsworld@gmail.com>
2022-12-26 19:29:52 +0630

    Update and rename console.log to consolelog.js

---
+++ package/functions/funcs/consolelog.js
@@ -0,0 +1,11 @@
+module.exports = async d => {
+const code = d.command.code
+const log = require('../../classes/Logger.js')
+const inside = d.unpack()
+
+console.log(inside.addBrackets())
+
+return {
+     code: code.replaceLast(`$consolelog${inside}`, "")
+     }
+     }
